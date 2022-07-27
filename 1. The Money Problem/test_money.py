05e795c6025e63f890a308eb30f7a37487eb12d5
Ramin Ferdos <ferdos.ramin@gmail.com>
2022-07-27 12:44:47 +0430

    addition of portoflio in same currency

--- 1. The Money Problem/test_money.py
+++ 1. The Money Problem/test_money.py
@@ -1,4 +1,6 @@
 import unittest
+import functools
+import operator
 
 
 class Money():
@@ -19,6 +21,18 @@ class Money():
             print('Division by Zero')
 
 
+class Portfolio():
+    def __init__(self, *moneys):
+        self.moneys = []
+
+    def add(self, *moneys):
+        self.moneys.extend(moneys)
+
+    def evaluate(self, currency):
+        total = functools.reduce(operator.add, map(lambda m: m.amount, self.moneys))
+        return Money(total, currency)
+
+
 class TestMoney(unittest.TestCase):
     def testMultiplicationInDollars(self):
         ten_dollars = Money(10, 'USD')
@@ -31,13 +45,21 @@ class TestMoney(unittest.TestCase):
         twenty_euros = Money(20, 'EUR')
         self.assertEqual(twenty_euros, ten_euros.times(2))
         self.assertEqual('EUR', twenty_euros.currency)
-    
+
     def testDivideMoney(self):
         korean_money = Money(4002, 'KRW')
         korean_money = korean_money.divide(4)
         expected_money = Money(1000.5, 'KRW')
         self.assertEqual(korean_money.amount, expected_money.amount)
         self.assertEqual(korean_money.currency, expected_money.currency)
+
+    def testAddition(self):
+        five_dollars = Money(5, 'USD')
+        ten_dollars = Money(10, 'USD')
+        fifteen_dollars = Money(15, 'USD')
+        portfolio = Portfolio()
+        portfolio.add(five_dollars, ten_dollars)
+        self.assertEqual(fifteen_dollars, portfolio.evaluate('USD'))
 
 
 if __name__ == '__main__':
